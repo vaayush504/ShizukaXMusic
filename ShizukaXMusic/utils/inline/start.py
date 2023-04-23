4b5e4ff330ed53873fe01918ccb9ef371c0cfab4
sᴛᴀʀ ʙᴏɪ <119308395+its-star-boi@users.noreply.github.com>
2023-04-23 23:33:03 +0530

    Update start.py

--- ShizukaXMusic/utils/inline/start.py
+++ ShizukaXMusic/utils/inline/start.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton
 
-from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+import config
 from ShizukaXMusic import app
 
 
